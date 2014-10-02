e6417d4d9b49dfe711fd9f511ef040d83f516ca6
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-10-02 16:46:44 -0400

    Checkin of analyzer to be used in test run.

--- thermoml/analyze_ml.py
+++ thermoml/analyze_ml.py
@@ -21,7 +21,7 @@ name_to_formula = pd.read_hdf("./compound_name_to_formula.h5", 'data')
 name_to_formula = name_to_formula.dropna()
 
 
-which_atoms = ["H","N","C","O","S","Cl","Br"]
+which_atoms = ["H","N","C","O"]#,"S","Cl","Br"]
 X = X[X["Temperature, K"] > 270]
 X = X[X["Temperature, K"] < 330]
 X = X[X["Pressure, kPa"] > 50.]
@@ -47,11 +47,11 @@ X = X[X.n_components == 1]
 X.dropna(axis=1, how='all', inplace=True)
 
 X["n_heavy_atoms"] = X.components.apply(lambda x: thermoml_lib.count_atoms(name_to_formula[x]))
-X = X[X.n_heavy_atoms <= 5]
+X = X[X.n_heavy_atoms <= 10]
 X.dropna(axis=1, how='all', inplace=True)
 
 X["n_atoms"] = X.components.apply(lambda x: thermoml_lib.count_atoms(name_to_formula[x], which_atoms=which_atoms))
-X = X[X.n_atoms <= 5]
+X = X[X.n_atoms <= 100]
 X.dropna(axis=1, how='all', inplace=True)
 
 X["smiles"] = X.components.apply(lambda x: cirpy.resolve(x, "smiles"))  # This should be cached via sklearn.
@@ -63,13 +63,15 @@ X["cas"] = X.components.apply(lambda x: thermoml_lib.get_first_entry(cirpy.resol
 X = X[X.cas != None]
 X = X.ix[X.cas.dropna().index]
 
-
+X["Temperature, K"] = np.round(X["Temperature, K"])
+X["Pressure, kPa"] = np.round(X["Pressure, kPa"])
 mu = X.groupby(["components", "smiles", "cas", "Temperature, K", "Pressure, kPa"])[experiments].mean()
 sigma = X.groupby(["components", "smiles", "cas", "Temperature, K", "Pressure, kPa"])[experiments].std().dropna()
 
+q = q[::100]
 q = mu.reset_index()
 #q.to_csv("./densities.csv")
 #q.to_csv("./sound.csv")
-q.to_csv("./data.csv")
+q.to_csv("./data_100x.csv")
 
 
